e14d979f3aaf433bf47bf88e0de7d11f2832addb
giauw.sk <gianvito.scandurra@stevejobs.academy>
2023-05-16 19:18:05 +0200

    try() useContext

--- src/App.tsx
+++ src/App.tsx
@@ -1,40 +1,52 @@
-import React, { useContext, useEffect } from 'react';
+import React, { createContext, useContext, useEffect, useState } from 'react';
 import './App.css';
-import { ThemeContext, ThemeContexts } from './components/Theme';
 import { NavbarC } from './components/Navbar';
 import { Sidebar } from './components/Sidebar';
 import { Outlet } from 'react-router-dom';
 import { Home } from './components/Home';
 import { Footer } from './components/Footer';
+import { ThemeProvider } from 'react-bootstrap';
+import { ThemeProviderr } from './components/Theme';
+
+interface MyContextType {
+  theme: string,
+  setTheme: React.Dispatch<React.SetStateAction<string>>,
+  toggleTheme: () => void
+}
+
+export const MyContext = createContext<MyContextType | undefined>(undefined);
 
 const App = () => {
-  const { theme, setTheme } = useContext(ThemeContext);
+
+  const [theme, setTheme] = useState<string>("light");
+
   const toggleTheme = () => {setTheme(theme === "light" ? "dark" : "light")}
+
+  const contextValues: MyContextType = {
+    theme,
+    setTheme,
+    toggleTheme
+  }
 
   const color = theme === "light" ? "#333" : "#FFF";
   const backgroundColor = theme === "light" ? "#FFF" : "#333";
 
   document.body.style.color = color;
-  document.body.style.backgroundColor = backgroundColor;
-  
+  document.body.style.backgroundColor = backgroundColor;  
 
   return (
     <>
+    <MyContext.Provider value={contextValues}>
     <NavbarC/>
-{/*     <div className="row">
-      <div>Hi friend!</div>
-      <button onClick={() => toggleTheme}>
-        Switch to {theme === 'light' ? 'dark' : 'light'} mode
-      </button>
-      </div> */}
-    
     <div className='row d'>
+    <ThemeProviderr/>
       <Sidebar/>
       <div className='col-12'>
         <Home/>
       </div>
       <Footer/>
     </div>
+    </MyContext.Provider>
     </>
   );
 };
--- src/components/Theme.tsx
+++ src/components/Theme.tsx
@@ -1,20 +1,28 @@
-import React, { useState } from "react";
+import React, { useContext, useState } from "react";
+import { MyContext } from "../App";
 
 export type Theme = "light" | "dark";
 
-export type ThemeContexts = { theme: Theme; setTheme: Function };
+/* export type ThemeContexts = { theme: Theme; setTheme: Function };
 
 export const ThemeContext = React.createContext<ThemeContexts>({} as ThemeContexts);
 
-type IProps = {children: React.ReactNode}
+type IProps = {children: React.ReactNode} */
 
-export const ThemeProvider = ({children}: IProps) => {
+export const ThemeProviderr = (/* IProps */) => {
 
-  const [theme, setTheme] = useState<Theme>("light");
+  const {theme, setTheme, toggleTheme} = useContext(MyContext)!;
 
   return (
-    <ThemeContext.Provider value={{ theme, setTheme }}>
-      {children}   
-    </ThemeContext.Provider>
+    <>    
+    <div className="big">
+    <h1>Hi friend!</h1>
+    <button onClick={() => (toggleTheme(),
+                            console.log(theme))}>
+      Switch to {theme === 'light' ? 'dark' : 'light'} mode
+    </button>    
+    </div>
+
+    </>
   );
 };
